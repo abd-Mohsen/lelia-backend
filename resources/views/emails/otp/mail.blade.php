79ab0273163ce4a4cd008505911dac385769b86d
Abd Moh <abdpuni6pusl9@gmail.com>
2024-05-06 18:10:25 +0300

    fixed mail layout (colors + rtl + image)

--- resources/views/emails/otp/mail.blade.php
+++ resources/views/emails/otp/mail.blade.php
@@ -1,14 +1,14 @@
-<x-mail::message dir="rtl">
+<x-mail::message>
 
 <div style="text-align: center;">
-    <img src="public\lelia_logo.jpg" alt="lelia logo" />
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT_XQzZZj9-FXsJdSHvIld34g9YVtjDwWGtWKJpclxYw&s" alt="lelia logo" width="100" height="100"/>
 </div>
+<br>
+<h1>رمز التحقق</h1>
 
-# رمز التحقق
+<h3>هذا هو رمز التحقق الخاص بك، لا تشاركه مع أحد</h3>
+<h2 dir="rtl">{{ $otp }}</h2>
 
-هذا هو رمز التحقق الخاص بك، لا تشاركه مع أحد <br>
-<p>{{ $otp }}</p>
-
-شكرا<br>
-فريق ليليا
+<h4 dir="rtl">شكرا</h4>
+<h4 dir="rtl">فريق ليليا</h4>
 </x-mail::message>
